1b5df328fff436829b09f690d686f2ba83b8e9ec
Pasindu Madhuwantha <126347762+PasinduOG@users.noreply.github.com>
2025-12-28 00:28:49 +0530

    Added login.fxml and LoginController and created login design

---
+++ src/main/java/edu/icet/controller/LoginController.java
@@ -0,0 +1,21 @@
+package edu.icet.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+}
