76688efedc8bb8e5085a99296897d7091894001b
Pasindu Madhuwantha <126347762+PasinduOG@users.noreply.github.com>
2025-12-27 23:16:47 +0530

    Initial commit

---
+++ src/main/java/edu/icet/Starter.java
@@ -0,0 +1,21 @@
+package edu.icet;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Login - Thoga Kade");
+        stage.setResizable(false);
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login.fxml"))));
+        stage.show();
+    }
+}
